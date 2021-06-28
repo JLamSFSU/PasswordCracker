18ef099572eff9c7843e55889dc1d78fffcf10e0
Justin Lam <jlam18@mail.sfsu.edu>
2021-06-28 15:59:54 -0700

    Allow the program to run multiple times through the use of goto

--- main.cpp
+++ main.cpp
@@ -1,17 +1,47 @@
 using namespace std;
 #include "panel.h"
 #include "pcrack.h"
+#include "pcompare.h"
 
 int main(void)
 {
 	string passwordToCrack;
 	string passwordFound;
+	char yn;
 
-	cout << "Enter Password to Crack: ";
+	START:cout << "Enter Password to Crack: ";
 	cin >> passwordToCrack;
 
-	mainMenu();
-	passwordFound = cracker(passwordToCrack);
+	switch (mainMenu())
+	{
+	case 0:
+		// other method goes here.
+		passwordFound = compare(passwordToCrack);
+	case 1:
+		passwordFound = cracker(passwordToCrack);
+		break;
+	case 2:
+		// other method goes here.
+		passwordFound = compare(passwordToCrack);
+		break;
+	}
 	cout << "Password is: " << passwordFound << endl;
+
+	do {
+		cout << "\n\nRestart? (1 for yes 0 for no): ";
+		cin >> yn;
+		cout << "yn:" << yn << endl;
+		switch (yn)
+		{
+		case '0':
+			return 0;
+		case '1':
+			cout << endl;
+			goto START;
+		default:
+			cout << "Bad Entry" << endl;
+			cin.clear();
+		}
+	} while (true);
 	return 0;
 }
