cab150c8ce4294cc7ad1cd0063e12089779f1d99
Justin Lam <jlam18@mail.sfsu.edu>
2021-06-29 18:12:50 -0700

    Rough implementation of list of existing passwords to check.

--- pcompare.cpp
+++ pcompare.cpp
@@ -5,29 +5,91 @@
 #include "pcompare.h"
 #include "passwordComponent.h"
 
+int getList(string[]);
+bool fileCondition = false;
+ifstream file;
+
+/**
+* https://www.passwordrandom.com/most-popular-passwords
+* @param passwordToCrack password to find
+* @return the password found.
+*/
 string compare(string passwordToCrack)
 {
 	cout << "Comparing..." << endl;
-	string myArray[6];
+	string myArray[100];
+	int sizeOfList;
 
-	ifstream file("file.txt");
-	if (file.good())
+	if (fileCondition == false)
 	{
-		cout << "file is open..." << endl;
-		for (int i = 0; i < 6; i++)
-		{
-			file >> myArray[i];
-		}
+		file.open("file.txt");
+		fileCondition = true;
 	}
 
-	if (file.good())
+
+
+	while (true)
 	{
-		for (int i = 0; i < 6; i++)
+		sizeOfList = getList(myArray);
+
+		for (int i = 0; i < sizeOfList; i++)
 		{
-			cout << myArray[i] << endl;
+			// cout << i <<") Checking: " << myArray[i] << endl;;
+			if (passwordToCrack.compare(myArray[i]) == 0)
+			{
+				file.close();
+				fileCondition = false;
+				return myArray[i];
+			}
+		}
+
+		if (sizeOfList == -1)
+		{
+			cout << "Password can not be found in list..." << endl;
+			file.close();
+			fileCondition = false;
+			return "";
 		}
 	}
-	file.close();
 
 	return "";
+}
+
+/**
+* Fills the list of prexisiting password to match with,
+* the list is populated up to 100 words and only stores
+* the words that match the parameters when initialized.
+* @param listOfP is a the list that is given back to compare
+* and find the password.
+* @return actual size of the list being passed back
+*/
+int getList(string listOfP[])
+{
+	int sizeOfList = 0;
+	bool regexCase = true;
+	string tempWord;
+	if (file.eof())
+		return -1;
+
+	if (!file.good())
+	{
+		cout << "error with file..." << endl;
+		return -1;
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		file >> tempWord;
+		if (regexCase)// pass regex store to list
+		{
+			listOfP[sizeOfList] = tempWord;
+			sizeOfList++;
+		}
+	}
+
+	// cout << "Printing file:" << endl;
+	// for (int i = 0; i < sizeOfList; i += 10)
+	// 	cout << listOfP[i] << endl;
+	
+	return sizeOfList;
 }
