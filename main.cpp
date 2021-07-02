4e031d18294dda9519ec85087ddf122e22c64775
Justin Lam <jlam18@mail.sfsu.edu>
2021-07-02 11:58:40 -0700

    Added comments on current method ,  now considering case of creating password.

--- main.cpp
+++ main.cpp
@@ -3,27 +3,32 @@ using namespace std;
 #include "pcrack.h"
 #include "pcompare.h"
 
+string passwordToBeFound(void);
+
 int main(void)
 {
 	string passwordToCrack;
 	string passwordFound;
 	char yn;
 
-	START:cout << "Enter Password to Crack: ";
-	cin >> passwordToCrack;
-
-	switch (mainMenu())
+	START:switch (mainMenu())
 	{
 	case 0:
-		// other method goes here.
+		passwordToCrack = passwordToBeFound();
 		passwordFound = compare(passwordToCrack);
+		passwordFound = cracker(passwordToCrack);
+		break;
 	case 1:
+		passwordToCrack = passwordToBeFound();
 		passwordFound = cracker(passwordToCrack);
 		break;
 	case 2:
-		// other method goes here.
+		passwordToCrack = passwordToBeFound();
 		passwordFound = compare(passwordToCrack);
 		break;
+	case 3:
+		// method to generate password.
+		// passwordFound =
 	}
 	if (passwordFound != "")
 		cout << "Password is: " << passwordFound << endl;
@@ -39,11 +44,32 @@ int main(void)
 			return 0;
 		case '1':
 			cout << endl;
+			passwordToCrack.clear();
+			passwordFound.clear();
 			goto START;
 		default:
 			cout << "Bad Entry" << endl;
+			// Below clears out bad entries
 			cin.clear();
+			cin.ignore(INT_MAX);
 		}
 	} while (true);
 	return 0;
+}
+
+
+/**
+* intermediate function in place of an actual interface for password
+* finding. Currently password of interest is supplied, and the program
+* works to determine if it can be easily found.
+* Future implementation would change it so that it would be able to
+* interface with desired platforms to try different passwords till
+* success.
+*/
+string passwordToBeFound()
+{
+	string passwordToCrack;
+	cout << "Enter Password to Crack: ";
+	cin >> passwordToCrack;
+	return passwordToCrack;
 }
