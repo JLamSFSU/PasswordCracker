e77f2f78efaaaf5ddc68892110cd180cc60399f0
Justin Lam <jlam18@mail.sfsu.edu>
2021-07-06 12:12:47 -0700

    modularize to reduce primary function profile.

--- createp.cpp
+++ createp.cpp
@@ -7,6 +7,7 @@
 #include "passwordComponent.h"
 
 int generateType(int);
+bool minimalReq(int[], int, int);
 char generateChar(int);
 char generateAC(void);
 char generateNum(void);
@@ -22,7 +23,6 @@ string createPassword(void)
 	int psize = pc.maxLength;
 	char* passwordArray = new char[psize];
 	int* arrayForPasswordType = new int[psize];
-	bool minType[3] = { false };
 	srand(time(0));
 
 	int type = 0;
@@ -42,52 +42,15 @@ string createPassword(void)
 	retry:for (int i = 0; i < psize; i++)
 		arrayForPasswordType[i] = generateType(type);
 	
+	// check to bypass minimal requirments in the password.
 	if (((type == 3 || type > 4) && psize < 2) ||
 		(type == 7 && psize < 3))
 		goto create;
 
-	// check there is min of each type
-	switch (type)
-	{
-	case 3:
-		minType[0] = true;
-		minType[1] = true;
-		break;
-	case 5:
-		minType[0] = true;
-		minType[2] = true;
-		break;
-	case 6:
-		minType[1] = true;
-		minType[2] = true;
-		break;
-	case 7:
-		minType[0] = true;
-		minType[1] = true;
-		minType[2] = true;
-		break;
-	}
-
-	if (!(type == 1 || type == 2 || type == 4))
-	{
-		for (int i = 0; i < psize; i++)
-		{
-			switch (arrayForPasswordType[i])
-			{
-			case 1:
-				minType[0] = false;
-				break;
-			case 2:
-				minType[1] = false;
-				break;
-			case 4:
-				minType[2] = false;
-				break;
-			}
-		}
-		if (minType[0] == true || minType[1] == true || minType[2] == true)
-			goto retry;
-	}
+	// return to regenerate a new password format if it fails minimal req. 
+	if (!(type == 1 || type == 2 || type == 4) &&
+		minimalReq(arrayForPasswordType, type, psize))
+		goto retry;
 
 	// in the portion below will fill in the rest.
 	create:for (int i = 0; i < psize; i++)
@@ -149,6 +112,59 @@ int generateType(int type)
 		}
 	}
 	return storedValue;
+}
+
+/**
+* @param arrayForPasswordType, the array of ints that store the types on char to
+* be use in the created password.
+* @param type, parameters required in the password.
+* @param psize max sixe of the password.
+* return true or false if the array fulfill the requirements.
+*/
+bool minimalReq(int arrayForPasswordType[], int type, int psize)
+{
+	bool minType[3] = { false };
+
+	// check there is min of each type
+	switch (type)
+	{
+	case 3:
+		minType[0] = true;
+		minType[1] = true;
+		break;
+	case 5:
+		minType[0] = true;
+		minType[2] = true;
+		break;
+	case 6:
+		minType[1] = true;
+		minType[2] = true;
+		break;
+	case 7:
+		minType[0] = true;
+		minType[1] = true;
+		minType[2] = true;
+		break;
+	}
+
+	for (int i = 0; i < psize; i++)
+	{
+		switch (arrayForPasswordType[i])
+		{
+		case 1:
+			minType[0] = false;
+			break;
+		case 2:
+			minType[1] = false;
+			break;
+		case 4:
+			minType[2] = false;
+			break;
+		}
+	}
+	if (minType[0] == true || minType[1] == true || minType[2] == true)
+		return true;
+	return false;
 }
 
 /**
